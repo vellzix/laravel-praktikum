dfaac1c1da66b105267c22a030c78a99c54d1835
vellzix <153348397+vellzix@users.noreply.github.com>
2025-05-24 00:29:42 +0800

    fix authcontroller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Foundation\Auth\User;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
